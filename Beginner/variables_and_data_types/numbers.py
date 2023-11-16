7528972ab4d767f7d777f82f24835ab526595cea
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-16 16:14:13 +0600

    Learn numbers

---
+++ Beginner/variables_and_data_types/numbers.py
@@ -0,0 +1,14 @@
+print(2) # 2
+print(2.5859) # 2.5859
+print(-2.899) # -2.899
+
+print(3 * 4 + 5) # 17
+print(3 * (4 + 5)) # 27
+
+print(10 % 3) # 1
+
+character_age = 28
+
+print(character_age) # 28
+print(str(character_age)) # "28"
+print("I am " + str(character_age) + " years old.")
