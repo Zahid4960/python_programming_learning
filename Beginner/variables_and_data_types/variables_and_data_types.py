b4e3d75b08da57e9a76a47ae2e417fb6c2c99eb1
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-16 15:44:19 +0600

    learned variables & data types

--- Beginner/variables_and_data_types/variables_and_data_types.py
+++ Beginner/variables_and_data_types/variables_and_data_types.py
@@ -1,11 +1,8 @@
 character_name = "Zahid"
 character_age = "28"
-character_salary = 60000.50
-is_male = true
 
-print("There is a man named " + character_name + ".")
-print("He is " + character_age + " years old.")
+print("There is a man named " + character_name + ". He is " + character_age + " years old.")
 
 character_name = "Ontor"
 character_age = "25"
-print("He liked his name " + character_name + " and age being like " + character_age + " years old.")
+print("He likex his name " + character_name + " and age being like " + character_age + " years old.")
