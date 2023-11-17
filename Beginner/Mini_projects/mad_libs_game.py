ca9cebc22428508f7a1c366a557c89d9ebd7a0ae
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-17 12:41:56 +0600

    Did mini project named mad libs game

---
+++ Beginner/Mini_projects/mad_libs_game.py
@@ -0,0 +1,7 @@
+fav_color = input("Enter your favourite color: ")
+fav_food = input("Enter your favourite food: ")
+fav_celeb = input("Enter yout fvaourite celebrity: ")
+
+print("Your favourite color: " + fav_color)
+print("Your favourite food: " + fav_food)
+print("Your favourite celebrity: " + fav_celeb)
