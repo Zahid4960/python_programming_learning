5dfc6e2a01d287261ab03a707cac4b9e3e84fc63
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-17 16:12:53 +0600

    Learn about functions & return statements

---
+++ Beginner/06_Functions/02_return_statement.py
@@ -0,0 +1,11 @@
+def square(number):
+    return number * number
+
+def cube(number):
+    return pow(number, 3)
+
+square_of_number = square(8)
+cube_of_number = cube(8)
+
+print(square_of_number)
+print(cube_of_number)
