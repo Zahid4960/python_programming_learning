98b75f749d636616ed148a63e0df1fafc667f44b
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-17 12:25:12 +0600

    Learn about user input

---
+++ Beginner/User_input/user_input.py
@@ -0,0 +1,4 @@
+character_name = input("Enter your character name: ")
+character_age = input("Enter your character age: ")
+
+print("Hello " + character_name + "!. Your age is: " + character_age + " years old.")
