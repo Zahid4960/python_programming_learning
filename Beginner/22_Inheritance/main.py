c0d6cd646371c374d935b32711385edb42edb38c
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-22 17:23:02 +0600

    Did some naming modification & learn about inheritance

---
+++ Beginner/22_Inheritance/main.py
@@ -0,0 +1,18 @@
+from software_engineer import SoftWareEngineer
+from software_engineer_web import SoftwareEngineerWeb
+
+SoftWare_Engineer_1 = SoftWareEngineer()
+SoftWare_Engineer_1.make_web_app()
+SoftWare_Engineer_1.make_ios_app()
+SoftWare_Engineer_1.make_android_app()
+SoftWare_Engineer_1.test_app()
+
+print("\n")
+
+SoftWare_Engineer_web_1 = SoftwareEngineerWeb()
+SoftWare_Engineer_web_1.make_web_app()
+SoftWare_Engineer_web_1.make_ios_app()
+SoftWare_Engineer_web_1.make_android_app()
+SoftWare_Engineer_web_1.test_app()
+SoftWare_Engineer_web_1.make_web_backend_app()
+SoftWare_Engineer_web_1.make_web_frontend_app()
---
+++ Beginner/22_Inheritance/software_engineer_web.py
@@ -0,0 +1,8 @@
+from software_engineer import SoftWareEngineer
+
+class SoftwareEngineerWeb(SoftWareEngineer):
+    def make_web_frontend_app(self):
+        print("This engineer make web frontend application!")
+
+    def make_web_backend_app(self):
+        print("This engineer make web backend application!")
